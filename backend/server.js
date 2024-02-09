a677f23c256f6c4bc5086c894b29462c72894e8e
reetukumari <poojamallick17@gmail.com>
2024-02-10 00:01:15 +0530

    todo

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const todoRoutes = require("./routes/todoRoutes");
+const userRoutes = require("./routes/userRoutes");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/todos", todoRoutes);
+app.use("/api/users", userRoutes);
+
+mongoose
+  .connect(process.env.DB_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db & listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log("error occured", error);
+  });
